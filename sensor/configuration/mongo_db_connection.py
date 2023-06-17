f68db08d84b8ba453343e35f7971407c3394021b
Yashwanth1119 <yashwanthtvv@gmail.com>
2023-06-17 14:52:58 +0530

    Data Ingestion Done

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -10,7 +10,7 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                #mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 mongo_db_url = "mongodb+srv://yash9596:Access1234@cluster0.bpqgi.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
