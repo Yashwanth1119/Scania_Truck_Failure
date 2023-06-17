5593b99b41cd15034b181c1cc7f2e67842147922
Yashwanth1119 <yashwanthtvv@gmail.com>
2023-06-17 13:24:32 +0530

    constant folder update

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
 
+
+def test_exception():
+    try:
+        logging.info("we are dividing 1 by zero")
+        x=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
 
 if __name__ == '__main__':
     mongodb_client = MongoDBClient()
     print("collection name:",mongodb_client.database.list_collection_names())
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+
+    #mongodb_client = MongoDBClient()
+    #print("collection name:",mongodb_client.database.list_collection_names())
