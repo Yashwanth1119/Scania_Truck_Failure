ad424f5f8f6ae0806ca7feb1479ea5aa69cd779e
Bunnydavid27 <bunnydavid378@gmail.com>
2023-07-16 17:02:01 +0530

    updated

--- main.py
+++ main.py
@@ -4,8 +4,44 @@ import os,sys
 from sensor.logger import logging
 from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
+from fastapi.responses import ORJSONResponse
+from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile
+from sensor.constant.application import APP_HOST, APP_PORT
+from starlette.responses import RedirectResponse
+from uvicorn import run as app_run
+from fastapi.responses import Response
+from sensor.utils.main_utils import load_object
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks, Form
+import csv
+import codecs
+import requests
+from fastapi import File, UploadFile
 
 
-if __name__ == '__main__':
-    training_pipeline = TrainPipeline()
-    training_pipeline.run_pipeline()
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", tags = ["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+@app.get("/train")
+async def train_route():
+    try:
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        return Response(f"Error Occured")
+    
